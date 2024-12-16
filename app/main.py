3d1c774033e68e878db419c0d84a351d3b3708c2
mmsalmanfaris <mmsalmanfaaris01@gmail.com>
2024-12-16 16:21:19 +0530

    delete and update

--- app/main.py
+++ app/main.py
@@ -22,6 +22,12 @@ def find_post(id):
             return p
 
 
+def find_index_post(id):
+    for i, p in enumerate(my_post):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 def root():
     return{"message": "Hello World!"}
@@ -32,7 +38,7 @@ def get_posts():
     return {"data": my_post}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0, 100000)
@@ -54,3 +60,30 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id:{id} was not found")
     return{"post_details" : post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post {id} not found")
+
+    my_post.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    print(post)
+
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post {id} not found")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_post[index] = post_dict
+
+    return {'date': post_dict}
